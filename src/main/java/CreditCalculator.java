861a8bc3bf907577cd911e80879ab4c67cb060c0
mrsBlonda <Anna89851004453@yandex.ru>
2022-08-10 22:51:54 +0300

    Реализация

--- src/main/java/CreditCalculator.java
+++ src/main/java/CreditCalculator.java
@@ -1,2 +1,13 @@
 public class CreditCalculator {
+    public static long monthlyPayment(long sumCredit, int paymentTerm, int interestRate) {
+        return ((sumCredit * (interestRate / 100 / 12) * paymentTerm) + sumCredit) * paymentTerm;
+    }
+
+    public static long totalAmount(long sumCredit, int paymentTerm, int interestRate) {
+        return (sumCredit * (interestRate / 100 / 12) * paymentTerm) + sumCredit;
+    }
+
+    public static long overPayment(long sumCredit, int paymentTerm, int interestRate) {
+        return sumCredit * (interestRate / 100 / 12) * paymentTerm;
+    }
 }
